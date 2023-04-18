f13a949da6e7b759e2946a0d7022f5e85e168cb1
mamismeda <amamismeda@gmail.com>
2023-04-18 21:22:30 +0400

    Weather app with autocomplete

---
+++ src/api.js
@@ -0,0 +1,12 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'f8cce2e5efmsh7ba2948e894a791p1a5b85jsn79cf490fff11',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo"
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5"
+export const WEATHER_API_KEY = "7bee5a3d187b8ea3c18f21008fb8e85b"
+
